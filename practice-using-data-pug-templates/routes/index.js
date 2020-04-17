566ce84089e24b3d0d482c272e89bb738f40f9d2
normanlew <normanlew@users.noreply.github.com>
2020-04-17 13:23:04 -0700

    First commit

---
+++ practice-using-data-pug-templates/routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const { recipes } = require('../data/data.json');
+// 6:19 for how pages should look when complete
+/* GET home page.    3:20 */
+router.get('/', function(req, res, next) {
+  // 1. Pass all recipe data to 'index' template
+  res.render('index', {recipes});
+});
+
+/* GET recipe page. 3:28 */
+router.get('/recipes/:id', function(req, res, next) {
+  const recipeId = req.params.id;  // Stores the value of an id route parameter; the id in the url path
+  const recipe = recipes.find( ({ id }) => id === +recipeId ); // holds the recipe object to pass into the view
+                  // finds the recipe object whose id value matches the id parameter
+  
+  if (recipe) {
+    // 2. Pass the recipe data to the 'recipe' template
+    res.render('recipe');
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+module.exports = router;
